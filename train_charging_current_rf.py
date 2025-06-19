ce5afc5c2eaa885d70d4059974c00ecbfe48a27a
Faustyna Misiura <110495453+faustyna77@users.noreply.github.com>
2025-06-19 03:28:16 +0200

    Update train_charging_current_rf.py

--- train_charging_current_rf.py
+++ train_charging_current_rf.py
@@ -33,7 +33,7 @@ def fetch_data(start_time, end_time):
     query = f'''
     from(bucket: "{INFLUX_BUCKET}")
         |> range(start: {start_time}, stop: {end_time})
-        |> filter(fn: (r) => r["_measurement"] == "dht_measurements")
+        |> filter(fn: (r) => r["_measurement"] == "env_measurements")
         |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
     '''
     df = client.query_api().query_data_frame(query)
